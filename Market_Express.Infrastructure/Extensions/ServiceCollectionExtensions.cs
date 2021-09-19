1462090c344753cbf48a6ac08092ad68e98d99b7
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-09-19 12:16:47 -0600

    Metodos de extension de ServiceCollection

---
+++ Market_Express.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Market_Express.Domain.Abstractions.Repositories;
+using Market_Express.Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Market_Express.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
--- Market_Express.Web/Startup.cs
+++ Market_Express.Web/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Market_Express.Infrastructure.Extensions;
 
 namespace Market_Express.Web
 {
@@ -20,13 +16,13 @@ namespace Market_Express.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddRepositories();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
