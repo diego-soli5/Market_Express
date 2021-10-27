8e453a930385317a1322959a976f7139602f794c
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-10-27 16:13:44 -0600

    Avance crear usuarios

--- Market_Express.Application/DTOs/AppUser/AppUserCreateDTO.cs
+++ Market_Express.Application/DTOs/AppUser/AppUserCreateDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Market_Express.Domain.Enumerations;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace Market_Express.Application.DTOs.AppUser
@@ -7,21 +8,27 @@ namespace Market_Express.Application.DTOs.AppUser
     {
         [Required(ErrorMessage = "El campo es obligatorio.")]
         [StringLength(40)]
+        [BindProperty(Name = "AppUser.Name")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "El campo es obligatorio.")]
+        [BindProperty(Name = "AppUser.Identification")]
         public string Identification { get; set; }
 
         [Required(ErrorMessage = "El campo es obligatorio.")]
-        [EmailAddress(ErrorMessage = "Ingresa una dirección correo electronico válida.")]
+        [EmailAddress(ErrorMessage = "Ingresa una dirección correo electrónico válida.")]
         [StringLength(40)]
+        [BindProperty(Name = "AppUser.Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "El campo es obligatorio.")]
         [StringLength(40)]
+        [BindProperty(Name = "AppUser.Phone")]
+        [Phone(ErrorMessage = "Ingresa un número de teléfono válido.")]
         public string Phone { get; set; }
 
         [Required(ErrorMessage = "El campo es obligatorio.")]
+        [BindProperty(Name = "AppUser.Type")]
         public AppUserType Type { get; set; }
 
 
