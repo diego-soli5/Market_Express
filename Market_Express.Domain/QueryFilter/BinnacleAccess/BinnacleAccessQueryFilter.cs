f8b1ddeac4865ef6f7e6fb0750358960e8bcc395
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-11-13 14:40:58 -0600

    BinnacleAccessIndexViewModel

---
+++ Market_Express.Domain/QueryFilter/BinnacleAccess/BinnacleAccessQueryFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Market_Express.Domain.QueryFilter.BinnacleAccess
+{
+    public class BinnacleAccessQueryFilter : PaginationQueryFilter
+    {
+        [BindProperty(Name = "StartDate")]
+        public DateTime? StartDate { get; set; }
+
+        [BindProperty(Name = "EndDate")]
+        public DateTime? EndDate { get; set; }
+
+        [BindProperty(Name = "User")]
+        public string User { get; set; }
+    }
+}
