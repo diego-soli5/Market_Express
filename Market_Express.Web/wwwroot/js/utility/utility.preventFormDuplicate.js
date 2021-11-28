bb4e1a62432887bf37ba554a4921fdc79864ba33
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-11-28 11:21:39 -0600

    utility.preventFormDuplicate.js

---
+++ Market_Express.Web/wwwroot/js/utility/utility.preventFormDuplicate.js
@@ -0,0 +1,13 @@
+﻿$(document).on('invalid-form.validate', 'form', function () {
+    var button = document.querySelector('#btnSubmit');
+    setTimeout(function () {
+        button.disabled = false;
+    }, 1);
+});
+
+$(document).on('submit', 'form', function () {
+    var button = document.querySelector('#btnSubmit');
+    setTimeout(function () {
+        button.disabled = true;
+    }, 0);
+});
